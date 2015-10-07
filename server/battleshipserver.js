52b7b6c516732cdb11b07ff08d0a2957702bdc3a
nikhilJain17 <nikhil.jain0987@gmail.com>
2015-10-06 22:49:13 -0400

    =(

--- server/battleshipserver.js
+++ server/battleshipserver.js
@@ -5,22 +5,22 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   // send the html file to be served up
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function() {
+http.listen(3000, function () {
   console.log('listening on *3000');
 });
 
 
 // When a device connects to the server
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
 
   console.log("A user connected.");
 
-  io.emit('test connection')
+  io.emit('test connection');
 
 
   /************************************************************
@@ -30,15 +30,15 @@ io.on('connection', function(socket) {
   ************************************************************/
 
   // 1. disconnecting from server
-socket.on('disconnect', function() {
-    console.log("A user disconnected")
+socket.on('disconnect', function () {
+    console.log("A user disconnected");
   });
 
 
-// 2. Confirming location of your ships
-socket.on('confirm positions', positions[], function() {
-  
-})
+// // 2. Confirming location of your ships
+// socket.on('confirm positions', positions[], function() {
+//
+// })
 
 
 
---
+++ server/client_test2/client_test2/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  client_test2
+//
+//  Created by d.d on 10/6/15.
+//  Copyright (c) 2015 Team7. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    
+}
